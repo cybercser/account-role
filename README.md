ddb5c11685f55312feb38d8167c364f68491bc1a
cybercser <cybercser@gmail.com>
2018-06-19 14:27:09 +0800

    replace base64 with url safe base64 for access token

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## 部署步骤
 
 1. root登录MySQL，source init.sql完成数据库初始化。
-2. 配置nginx反向代理保证负载均衡，或者使用pm2负载均衡。
+2. 配置nginx反向代理或者使用pm2负载均衡。
 3. 配置https保证安全通信。
 
 ## 逻辑概要
@@ -11,7 +11,7 @@
 * 客户端登录游戏服务器之前，以上次登录的账号http GET查询其角色信息，显示于客户端“选择服务器-已有角色”界面。
 * 游戏中创建角色后后，客户端http POST角色数据到account-role服务器。
 * 游戏中角色数据更新（改名、升级、合服）时，客户端http PUT角色数据到account-role服务器。
-* RESTful API access_token = sha1(config.key+account_id).digest().toString('base64')。
+* RESTful API access_token = URLSafeBase64.encode(sha1(config.key+account_id).digest())。
 
 ## 注意事项
 
--- controllers/token.js
+++ controllers/token.js
@@ -1,12 +1,13 @@
 const APIError = require('../rest').APIError;
 const crypto = require('crypto');
+const URLSafeBase64 = require('urlsafe-base64');
 const config = require("../config");
 
 let validateToken = function(token, param) {
     var sha1 = crypto.createHash('sha1');
     sha1.update(config.key);
     sha1.update(param);
-    if (token != sha1.digest().toString('base64')) {
+    if (!URLSafeBase64.validate(token) || token != URLSafeBase64.encode(sha1.digest())) {
         console.log('[WARNING] invalid access token');
         throw new APIError('roles:not_found', 'invalid access token.');
     }
